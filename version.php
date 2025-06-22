87e92793f10947d89a83eef1cfab897c1e47427b
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-06-22 00:16:47 -0300

    release: 20250621068

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_suap';
-$plugin->release = '0.3.067';
-$plugin->version = 2025_06_05_067;
+$plugin->release = '0.3.068';
+$plugin->version = 20250621068;
 $plugin->maturity = MATURITY_STABLE;
 
-$plugin->requires = 2021_05_17_00;
+$plugin->requires = 2021051700;
